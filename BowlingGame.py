0ad37fc20435a3f12f3fcbeacdf9068d5d450a38
Pat2021 <84110006+Pat2021@users.noreply.github.com>
2023-09-27 13:57:07 -0300

    Create test_unitted. py
    Create a test file

--- BowlingGame.py
+++ BowlingGame.py
@@ -1,5 +1,3 @@
-#File 2 (BowlingGame.py)
-#This file has information about Bowling Game for which the description is provided in project assessment.
 
 class BowlingGame:
     def __init__(self):
@@ -10,10 +8,10 @@ class BowlingGame:
 
     def score(self):
         result = 0
-        rollIndex=0
+        rollIndex = 0
         for frameIndex in range(10):
-            if frameIndex in range(10):
-                result += self.StrikeScore(rollIndex)
+            if self.isStrike(rollIndex):
+                result += self.strikeScore(rollIndex)
                 rollIndex +=1
             elif self.isSpare(rollIndex):
                 result += self.spareScore(rollIndex)
@@ -27,21 +25,14 @@ class BowlingGame:
         return self.rolls[rollIndex] == 10
     def isSpare(self, rollIndex):
         return self.rolls[rollIndex]+ self.rolls[rollIndex+1]==10
-    def stickeScore(self,rollIndex):
-        return  10+ self.rolls[rollIndex+1]+ self.rolls[rollIndex+2]
-
-    def spareScore(self,rollIndex):
-        return  10+ self.rolls[rollIndex+2]
-
+    def strikeScore(self,rollIndex):
+        return  10 + self.rolls[rollIndex + 1]+ self.rolls[rollIndex + 2]
+    
+    def spareScore(self, rollIndex):
+        return 10 + self.rolls[rollIndex + 2]
+    
     def frameScore(self, rollIndex):
         return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
-		
+    
 
-#Your tasks for code parts:
-#1: If there are any bugs in the code, you have to remove using debugging and run the project and test cases.
-#2: Refactor the code (Improve its structure without changing external behaviour).
-#3: Report everything using github commits and versioning control.
-
-
-###### Important #####
 # Please complete your project and all tasks according to assessment description provided in CANVAS.
--- test_unitted.py
+++ test_unitted.py
@@ -1,38 +1,63 @@
-import BowlingGame
 import unittest
+from BowlingGame import BowlingGame
+# Import the BowlingGame class from your code
 
 class TestBowlingGame(unittest.TestCase):
 
     def setUp(self):
-        self.game = BowlingGame.BowlingGame()
+         # Initialize a BowlingGame instance for testing
+        self.game = BowlingGame()
 
     def testGutterGame(self):
-        for i in range(0, 20):
-            self.game.rolls(0)
-        assert self.game.score()==0
+        # Test a game where no pins are knocked down (a gutter game)
+        for i in range(0, 20): # 0 - 20 rolls with no pins down
+            self.game.roll(0)
+        
+        self.assertEqual(self.game.score(), 0)
+    
     def testAllOnes(self):
-        self.rollMany(1, 20)
-        assert self.game.score()==20
-    def testOneSpare(self):
-        self.game.rolls(5)
-        self.game.rolls(5)
-        self.game.rolls(3)
-        self.rollMany(0,17)
-        assert self.game.score()==16
-    def testOneStrike(self):
-        self.game.rolls(10)
-        self.game.rolls(4)
-        self.game.rolls(3)
-        self.rollMany(0,16)
-        assert  self.game.score()==24
-    def testPerfectGame(self):
-        self.rollMany(10,12)
-        assert self.game.score()==300
-    def testOneSpare(self):
-        self.rollMany(5,21)
-        assert self.game.score()==150
-    def rollMany(self, pins,rolls):
-        for i in range(rolls):
-            self.game.rolls(pins)
+        # Test a game where one pin is knocked down in each roll
+        self.rollMany(1, 20) # 1 - 20 rolls with 1 pin down each time
 
+    def rollMany(self, pins, rolls):
+        for i in range(rolls):
+            self.game.roll(pins)
+
+        self.assertEqual(self.game.score(), 20)
+
+    def testOneSpare(self):
+        # Test a game with spares
+        self.game.roll(5)
+        self.game.roll(5) # Spare
+        self.game.roll(3) # Bonus roll
+        self.rollMany(0, 17)  # Rolls with no pins down
+
+
+        self.assertEqual(self.game.score(), 13) # 10 (spare) + 3 (frame 2)
+
+    def testOneStrike(self):
+        # Test a game with strikes
+        self.game.roll(10) # Strike
+        self.game.roll(4)  # Bonus roll 2
+        self.game.roll(3)  # Bonus roll 1
+        self.rollMany(0, 16) #Rolls with no pins down
+
+        self.assertEqual(self.game.score(),17)
+
+    def testPerfectGame(self):
+        # Test a perfect game with all strikes
+        self.rollMany(10, 12) # 12 strikes in a row
+
+        self.assertEqual(self.game.score(), 30)
+
+    def testAllFives(self):
+        self.rollMany(5, 21) # Roll 5 pins 21 times
+
+        self.assertEqual(self.game.score(), 15)
+
+    def rollMany(self, pins, rolls):
+        for i in range(rolls):
+            self.game.roll(pins)
+# Use self.game.roll to simulate rolls
+if __name__ == '__main__':
             unittest.main()
